4697d13fce7d6fa83a7fc1bc76fbb7e36f534a24
Krasus1966 <2134123985@qq.com>
2020-10-21 17:39:04 +0800

    增加下拉菜单组件

--- src/components/GlobalHeader.vue
+++ src/components/GlobalHeader.vue
@@ -5,15 +5,20 @@
       <li class="list-inline-item"><a href="#" class="btn btn-outline-light my-2">登陆</a></li>
       <li class="list-inline-item"><a href="#" class="btn btn-outline-light my-2">注册</a></li>
     </ul>
-    <ul v-else class="list-inline mb-0">
-      <li class="list-inline-item"><a href="#" class="btn btn-outline-light my-2">你好 {{user.name}}</a></li>
+    <ul v-else class="list-inline mb-0" >
+      <dropdown :title="`你好 ${user.name}`">
+        <dropdown-item><a href="#" class="dropdown-item">新建文章</a></dropdown-item>
+        <dropdown-item disabled><a href="#" class="dropdown-item">编辑资料</a></dropdown-item>
+        <dropdown-item><a href="#" class="dropdown-item">退出登录</a></dropdown-item>
+      </dropdown>
     </ul>
   </nav>
 </template>
 
 <script lang="ts">
 import {defineComponent, PropType} from 'vue'
-
+import Dropdown from './Dropdown.vue'
+import DropdownItem from './DropdownItem.vue'
 export interface UserProps {
   isLogin: boolean;
   name?: string;
@@ -22,6 +27,10 @@ export interface UserProps {
 
 export default defineComponent({
   name: "GlobalHeader",
+  components: {
+    Dropdown,
+    DropdownItem
+  },
   props: {
     user: {
       type: Object as PropType<UserProps>,
